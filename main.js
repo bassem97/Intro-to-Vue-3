2bd0392398ebdf95713f00347edb0ba8ca682e47
BASSEM <bassem.jadoui@esprit.tn>
2023-03-22 12:50:01 +0100

    class & style binding

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
       cart: 0,
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
+      inStock: false,
       inventory: 11,
       onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
@@ -12,6 +13,9 @@ const app = Vue.createApp({
         { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
       ],
       sizes: ["S", "M", "L", "XL"],
+      isActive: false,
+      activeClass: "active",
+      selectedVariant: 2234,
 
       description: "A pair of warm, fuzzy socks",
       url: "https://www.google.com",
@@ -24,8 +28,9 @@ const app = Vue.createApp({
     removeFromCart() {
       this.cart === 0 ? (this.cart = 0) : (this.cart -= 1);
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateImage(variant) {
+      this.image = variant.image;
+      this.selectedVariant = this.variants.find((v) => v.id === variant.id).id;
     },
   },
 });
