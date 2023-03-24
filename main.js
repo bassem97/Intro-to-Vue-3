26de7431ed20e69d5ec6b8624fda8ae50acd21e9
BASSEM <bassem.jadoui@esprit.tn>
2023-03-24 09:12:21 +0100

    code challenge array v-for rendering

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const app = Vue.createApp({
       image: "./assets/images/socks_green.jpg",
       inventory: 11,
       onSale: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+      sizes: ["S", "M", "L", "XL"],
 
       description: "A pair of warm, fuzzy socks",
       url: "https://www.google.com",
