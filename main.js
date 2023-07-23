26a8f2605567cafb01feeff42fb3466ea4afbed4
BASSEM <bassem.jadoui@esprit.tn>
2023-07-24 00:48:04 +0100

    compile errors

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const app = Vue.createApp({
-    data(){
-        return  {
-            product: 'Socks',
-            description: 'A pair of warm, fuzzy socks',
-            image: './assets/images/socks_green.jpg',
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_green.jpg",
+      description: "A pair of warm, fuzzy socks",
+      url: "https://www.google.com",
+    };
+  },
+});
