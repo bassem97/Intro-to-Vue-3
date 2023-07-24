efa53b361582399a5fb6c4f04d74e06f3e227e59
BASSEM <bassem.jadoui@esprit.tn>
2023-07-24 01:10:54 +0100

    init npm repo to install and apply prettier

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
+      inventory: 100,
+
       description: "A pair of warm, fuzzy socks",
       url: "https://www.google.com",
     };
