ab083acb4ee1b6a7be6fde0323fa4cf0f75aaec5
BASSEM <bassem.jadoui@esprit.tn>
2023-07-24 01:15:09 +0100

    conditional rendering

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
-      inventory: 100,
+      inventory: 1,
 
       description: "A pair of warm, fuzzy socks",
       url: "https://www.google.com",
